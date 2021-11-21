4d3565b9405cbe9d6a0a068157a2fc1d48133af4
Power-tile <hejizheng2004@163.com>
2021-11-21 17:29:56 -0600

    Add RenderEngine, rewrite Map::Render

--- src/Map.cpp
+++ src/Map.cpp
@@ -3,13 +3,22 @@
 Map::Map() : shapes_(std::vector<MapShape*>()), items_(std::vector<Item*>()) {}
 
 void Map::Render(LevelPos center) {
-  // for (MapShape* shape : shapes_) {
-  //   shape->Draw(center);
-  // }
+  ScreenPos screen_center = {60, 60}; // TODO: relate this to UI initialization
+  for (MapShape* shape : shapes_) {
+    LevelPos shape_pos = shape->GetPosition();
+    if (shape_pos.level == center.level) {
+      LevelPos relative_pos = shape_pos - center;
+      shape->Draw({screen_center.x + relative_pos.x, screen_center.y + relative_pos.y});
+    }
+  }
 
-  // for (Item* item : items_) {
-  //   item->Draw(center);
-  // }
+  for (Item* item : items_) {
+    LevelPos item_pos = item->GetPosition();
+    if (item_pos.level == center.level) {
+      LevelPos relative_pos = item_pos - center;
+      item->Draw({screen_center.x + relative_pos.x, screen_center.y + relative_pos.y});
+    }
+  }
 }
 
 Item* Map::GetItem(LevelPos position) {
