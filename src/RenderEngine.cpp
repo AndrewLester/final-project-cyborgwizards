32b6adf63a5568c47a1e454ee897e02334e8a3e6
CalebLair <calebsl2@illinois.edu>
2021-11-27 16:09:51 -0600

    RenderAll worked on

--- src/RenderEngine.cpp
+++ src/RenderEngine.cpp
@@ -2,14 +2,14 @@
 
 void RenderEngine::SetChar(ScreenPos pos, char c) {
     TCODConsole::root->putChar(pos.x,pos.y,c);
-    // console.TCOD_console_set_char(console,pos.x,pos.y,c);
+    // TCOD_console_set_char(console,pos.x,pos.y,c);
 }
 
 void RenderEngine::SetCharCol(ScreenPos pos, char c, TCODColor col) {
   TCODConsole::root->putChar(pos.x,pos.y,c);
   TCODConsole::root->setCharForeground(pos.x,pos.y,col);
-  // console.TCOD_console_set_char(console,pos.x,pos.y,c);
-  // console.TCOD_console_set_char_foreground(console,pos.x,pos.y,c);
+  // TCOD_console_set_char(console,pos.x,pos.y,c);
+  // TCOD_console_set_char_foreground(console,pos.x,pos.y,c);
   // static constexpr auto TEAL = tcod::ColorRGB{0, 255, 255}; // color
 }
 
@@ -17,7 +17,7 @@ void RenderEngine::RoomFill(ScreenPos upleft, ScreenPos lowright, char c) {
     for (int x = upleft.x; x <= lowright.x; x++) {
         for (int y = upleft.y; y <= lowright.y; y++) {
             TCODConsole::root->putChar(x,y,c);
-            // console.TCOD_console_set_char(console,x,y,c);
+            // TCOD_console_set_char(console,x,y,c);
         }
     }
 }
@@ -27,7 +27,7 @@ void RenderEngine::RoomFillCol(ScreenPos upleft, ScreenPos lowright, char c, TCO
         for (int y = upleft.y; y <= lowright.y; y++) {
             TCODConsole::root->putChar(x,y,c);
             TCODConsole::root->setCharForeground(x,y,col);
-            // console.TCOD_console_set_char(console,x,y,c);
+            // TCOD_console_set_char(console,x,y,c);
         }
     }
 }
--- src/UI.cpp
+++ src/UI.cpp
@@ -15,8 +15,20 @@ UI::~UI() {
 }
   
 void UI::RenderAll() {
-    // draw the player in the middle of the screen, also map, monster, items and additional UI info
-    
+    // draw the player in the middle of the screen, also map, monster, items and additional UI info as needed
+    // RenderEngine screen = RenderEngine();
+    TCODConsole test = TCODConsole(80, 50); 
+    int width = test.getWidth();
+    int height = test.getHeight();
+
+    // auto console = tcod::Console{80, 50};  // get console from main? (have width/height be vars in main instead?)
+    // int width = TCOD_console_get_width(console);
+    // int height = TCOD_console_get_height(console);
+
+    struct ScreenPos center = {width / 2, height / 2}; // if even/odd how to define center?
+    player_->Draw(center);  // screen.SetChar(center, '@');  instead? 
+
+    // TCOD_console_flush() or TCODConsole::flush() needed after this step
 }
   
 Player* UI::GetPlayer() {
