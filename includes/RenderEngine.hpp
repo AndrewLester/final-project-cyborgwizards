d065388eed309078974aeba6914ac2268762d660
CalebLair <calebsl2@illinois.edu>
2021-12-04 12:43:31 -0600

    commit changes

--- includes/RenderEngine.hpp
+++ includes/RenderEngine.hpp
@@ -17,6 +17,10 @@ class RenderEngine {
   void SetCharCol(ScreenPos pos, char c, TCODColor col);
   void RoomFill(ScreenPos upleft, ScreenPos lowright, char c);
   void RoomFillCol(ScreenPos upleft, ScreenPos lowright, char c, TCODColor col);
+  // add one for walls vs play space in room?
+  // void Print()
+  // tcod::print(console, {0, 0}, "Hello World", TCOD_white, std::nullopt);
+  // TCODConsole::root->print(0, height-3, "Floor:");
 };
 
 #endif
--- src/UI.cpp
+++ src/UI.cpp
@@ -16,6 +16,7 @@ UI::~UI() {
   
 void UI::RenderAll() {
     // draw the player in the middle of the screen, also map, monster, items and additional UI info as needed
+
     // RenderEngine screen = RenderEngine();
     TCODConsole test = TCODConsole(80, 50); 
     int width = test.getWidth();
@@ -25,8 +26,18 @@ void UI::RenderAll() {
     // int width = TCOD_console_get_width(console);
     // int height = TCOD_console_get_height(console);
 
-    struct ScreenPos center = {width / 2, height / 2}; // if even/odd how to define center?
-    player_->Draw(center);  // screen.SetChar(center, '@');  instead? 
+    struct ScreenPos center1 = {width / 2, height / 2}; // if even/odd how to define center?
+    struct LevelPos center2 = {width / 2, height / 2, 0}; // initial level but change to make abitrary
+    map_->Render(center2);
+    player_->Draw(center1);
+    monster_->Draw(center1);
+
+    // how to draw inv to screen, player health, 
+    // tcod::print(console, {0, 0}, "Hello World", TCOD_white, std::nullopt);
+    // or TCODConsole::root->print(0, y, "text");
+    // pull and merge main branch, see getter for player hp, level #?
+
+    // use gamestate to determine what to render, set up win and lost games too
 
     // TCOD_console_flush() or TCODConsole::flush() needed after this step
 }
