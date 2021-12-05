eeffa0f8cc65b51d69281da4823397dc0de865a2
CalebLair <calebsl2@illinois.edu>
2021-12-05 15:25:14 -0600

    the console works now

--- src/main.cpp
+++ src/main.cpp
@@ -9,7 +9,6 @@
 #include "UI.hpp"
 
 int main(int argc, char** argv) {
-  // std::cout << "HI" << std::endl;
   auto params = TCOD_ContextParams{};
   params.tcod_version = TCOD_COMPILEDVERSION;
   params.argc = argc;
@@ -20,18 +19,32 @@ int main(int argc, char** argv) {
 
   auto console = tcod::Console{80, 25};
   params.console = console.get();
-  // RenderEngine::Instance().SetConsole(&console);
 
   auto context = tcod::new_context(params);
+
+  // RenderEngine::Instance().SetConsole(&console);
   // UI::Instance().RenderAll();
+
+  // depricated code that works 
+  // TCODConsole::initRoot(80, 25, "title", false);
 
   // Game loop.
   while (true) {
     //   // Rendering.
+
+    // TCOD_console_clear(console.get());
+    // tcod::print(console, {0, 0}, "Hello World", std::nullopt, std::nullopt);
+    // context->present(console);
+
     console.clear();
-    // tcod::print(console, {0, 0}, "Hello World", TCOD_white, std::nullopt);
-    TCOD_console_flush();
+    tcod::print(console, {0, 0}, "Hello World", TCOD_white, std::nullopt);
     context->present(console);
+    // TCOD_console_flush();
+
+    // depricated code that works 
+    // TCODConsole::root->clear();
+    // TCODConsole::root->print(0, 0, "Hello World");
+    // TCODConsole::root->flush();
 
     // Handle input.
     SDL_Event event;
