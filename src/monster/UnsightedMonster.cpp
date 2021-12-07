4657aaf4d1a9d4e9683053c4526c64ce29a6fbd8
Power-tile <hejizheng2004@163.com>
2021-12-07 03:53:36 -0600

    Fix divide by zero problem

--- src/monster/UnsightedMonster.cpp
+++ src/monster/UnsightedMonster.cpp
@@ -16,15 +16,16 @@ void UnsightedMonster::OnNotify(Event* event) {
   if (event->emitter_ != this && event->GetType() == "SoundEvent") {
     SoundEvent sound_event = *dynamic_cast<SoundEvent*>(event);
     int rel_intensity = sound_event.RelativeIntensity(this->position_);
-    if (rel_intensity > 90) { // directly change
+    if (rel_intensity > 97) { // directly change
       this->prev_intensity_ = rel_intensity;
       this->ChangeDestination(sound_event.source);
       this->state_ = MonsterState::Chase;
     } else if (rel_intensity > this->prev_intensity_ && rel_intensity > 20) { // uncertain
       this->prev_intensity_ = rel_intensity;
       auto rooms = UI::Instance().GetMap()->GetRoomsInRadius(this->position_, (100 - rel_intensity) / 10);
-      int rand_idx = rand() % rooms.size();
-      this->ChangeDestination(rooms.at(rand_idx)->GetCenterPosition());
+      if (rooms.size() == 0) return;
+      auto target = rooms.at(rand() % rooms.size());
+      this->ChangeDestination(target->GetCenterPosition());
       this->state_ = MonsterState::Roam;
     } // else: too small, don't react
   }
